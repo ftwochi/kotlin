d9c306f244ec7d263be38918d50bf583b2648288
ftwochi <157507982+ftwochi@users.noreply.github.com>
2024-01-30 10:23:41 -0600

    Create checkpoint.kt

---
+++ checkpoint.kt
@@ -0,0 +1,28 @@
+const val HERO_NAME = "Madrigal"
+fun main() {
+    println("The hero announces her presence to the world.")
+
+    println(HERO_NAME)
+    var playerLevel = 4
+    printl(playerLevel)
+
+    if (playerLevel == 1) {
+        println("Meet Mr. Bubbles in the land of soft things.")
+    } else if (playerLevel <= 5) {
+            println("Save the town from the barbatian invasions.")
+        } else if(playerLevel == 6) {
+        println("Locate the enchanted sword.")
+    } else if (playerLevel == 7) {
+        println("Recover the long-lost artifact of creation.")
+    } else if (playerLevel == 8) {
+        println("Defeat Nogartse, bringer of death and eater of worlds.")
+    } else {
+        println("There are no quests right now.")
+    }
+
+    println("Time passes...")
+    println("The hero returns for her quest.")
+
+    playerLevel += 1
+    println(playerLevel1)
+}
