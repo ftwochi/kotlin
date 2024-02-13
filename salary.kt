3f3f4bea20f030aa20ba49a3db33c9ebe3b10ceb
ftwochi <157507982+ftwochi@users.noreply.github.com>
2024-02-13 11:03:11 -0600

    Create salary.kt

---
+++ salary.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var name: String = "Adriana Hernandez"
+    var age: Int = 15
+    var annualPay: Double = 70,000,000
+
+    println("my name is " + name + ", my age is " + age + ".")
+}
