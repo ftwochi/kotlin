cbc502d8eabb5f01ec95e5b0399eb4259c090aa7
ftwochi <157507982+ftwochi@users.noreply.github.com>
2024-02-15 10:27:25 -0600

    Create marketing.kt

---
+++ marketing.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    val extrovert = "500"
+    val introvert = "15"
+    if (extrovert < introvert ) {
+        println("You prefer dogs?")
+    } else {
+        println("Are you a cat person?")
+    }
+}
