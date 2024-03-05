fb96eb86050d2f889d6e7859d36bd64bedbf1b59
ftwochi <157507982+ftwochi@users.noreply.github.com>
2024-03-05 10:34:12 -0600

    Create priceCalculator.kt

---
+++ priceCalculator.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val wholesale=5
+     if (wholesale in 6..4){
+         println("wholesale price is 5")
+     } else if(wholesale in 6..4){
+         println("wholesale price is 5")
+     } else{
+         println("wholesale price is not 5")
+     }
+}
